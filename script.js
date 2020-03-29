2c5b95c1f3b993891697c7841cecbce8e5357db9
Vyacheslav Manzhela <wmanzhela@gmail.com>
2020-03-29 18:19:33 +0300

    feat: add draft code to script.js

--- script.js
+++ script.js
@@ -1,107 +1,193 @@
+/* Constants */
+const MENU = document.getElementById('menu');
 
-let ul = document.querySelector('.menu');
-let li = document.querySelectorAll('.menu-item');
-let ulTwo = document.querySelector('.menu-two');
-let liTwo = document.querySelectorAll('.menu-two-item');
-let imageBox = document.querySelector('.images');
-let images = document.querySelectorAll('.image');
-let phoneVertical = document.querySelector(".phone-vertical");
-let layerVertical = document.querySelector(".layer-vertical");
-let phoneHorizontal = document.querySelector(".phone-horizontal");
-let layerHorizontal = document.querySelector(".layer-horizontal");
-const btn = document.getElementById('btn');
-const btnClose = document.getElementById('close-btn');
+const SLIDER = document.getElementById('home');
+const BUTTON_LEFT= document.getElementById('btn-left');
+const BUTTON_RIGHT= document.getElementById('btn-right');
+const PHONE_V = document.getElementById('phone-vertical');
+const PHONE_H = document.getElementById('phone-horizontal');
 
-// Header
-li.forEach(el => {
-  el.addEventListener('click',()=>{
-      ul.querySelector('.active').classList.remove('active');
-      el.classList.add('active')
-  });
-});
+const PORTFOLIO_MENU = document.getElementById('portfolio-btn');
+const PHOTOS = document.getElementById('images');
 
-//Portfolio. Взаимодействие с картинками
-imageBox.addEventListener('click',(event) =>{
-  images.forEach(el => el.classList.remove('img-border'));
-  event.target.classList.add('img-border')
-});
+const SUBMIT = document.getElementById('submit');
+const CLOSE_BUTTON = document.getElementById('close-btn');
 
-liTwo.forEach(el => {
-  el.addEventListener('click',()=>{
-      ulTwo.querySelector('.two').classList.remove('two');
-      el.classList.add('two');
+/* Scroll menu on page */
+document.addEventListener('scroll', onScroll);
 
-      /*let d = document.createDocumentFragment();
-      d.appendChild(images[Math.floor(Math.random() * images.length)]);
-      imageBox.appendChild(d);*/
-      let divs = imageBox.children;
-      let frag = document.createDocumentFragment();
-      while (divs.length) {
-        frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
+function onScroll(event){
+  const curPos = window.scrollY;
+  const divs = document.querySelectorAll('.wrapper > div');
+  const links = document.querySelectorAll('#menu a');
+
+  divs.forEach( el => {
+    if(el.offsetTop - 80 <= curPos 
+        &&
+      (el.offsetTop + el.offsetHeight ) > curPos){
+          links.forEach( a => {
+          a.classList.remove('active');
+          if(el.getAttribute('id') === a.getAttribute('href').substring(1)){
+            a.classList.add('active');
+          }
+        });
     }
-       imageBox.appendChild(frag);
   });
-});
-
-// Get a Quote
-btn.addEventListener('click',() =>{
-  const subject = document.getElementById('subject').value.toString();
-  const description = document.getElementById('description').value.toString();
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  if(subject == ''){
-    document.getElementById('result').innerText = "Без темы";
-    document.getElementById('message-block').classList.remove('hidden');
-  }
-  else{
-    document.getElementById('result').innerText = subject;
-    document.getElementById('message-block').classList.remove('hidden');
-  }
-
-  if(description == ''){
-    document.getElementById('result-two').innerText = "Без темы";
-    document.getElementById('message-block').classList.remove('hidden');
-  }
-  else{
-    document.getElementById('result-two').innerText = subject;
-    document.getElementById('message-block').classList.remove('hidden');
-  }
-
-  if(name == '' || email == ''){
-    document.getElementById('message-block').classList.add('hidden');
-  }
-});
-
-btnClose.addEventListener('click',() =>{
-  document.getElementById('result').innerText = '';
-  document.getElementById('message-block').classList.add('hidden');
-  document.getElementById('form').reset();
-});
-
-// Phone Turn Off
-
-phoneVertical.addEventListener('click',() => layerVertical.style.display = layerVertical.style.display!='block' ? 'block' : 'none');
-
-phoneHorizontal.addEventListener('click',() => layerHorizontal.style.display = layerHorizontal.style.display!='block' ? 'block' : 'none');
-
- // Caroussel
-
-var slideIndex = [1,1];
-var slideId = ["slide"]
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
+
+/* Header */
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('li > a').forEach(el => { el.classList.remove('active') });
+    event.target.classList.add('active');
+})
+
+
+/* Slider */
+function changeSlider(){
+    offAllScreens();
+    if(!PHONE_H.hidden) {
+        PHONE_V.hidden = true;
+        PHONE_H.hidden = true;
+        SLIDER.classList.add('slider-panel-second');
+    }
+    else {
+        PHONE_V.hidden = false;
+        PHONE_H.hidden = false;
+        SLIDER.classList.remove('slider-panel-second');
+    }
 }
+
+BUTTON_LEFT.addEventListener('click', changeSlider);
+BUTTON_RIGHT.addEventListener('click', changeSlider);
+
+function offVerticalScreen() {
+    const verticalScreen = document.querySelector('.off-vertical');
+    if( verticalScreen.style.visibility !== "visible" ) {
+        verticalScreen.style.visibility = "visible";
+    } 
+    else verticalScreen.style.visibility = "hidden";
+}
+
+function offHorizontalScreen() {
+    const horizontalScreen = document.querySelector('.off-horizontal');
+    if( horizontalScreen.style.visibility !== "visible" ) {
+        horizontalScreen.style.visibility = "visible";
+    } 
+    else horizontalScreen.style.visibility = "hidden";
+}
+
+function offAllScreens(){
+    const verticalScreen = document.querySelector('.off-vertical');
+    const horizontalScreen = document.querySelector('.off-horizontal');
+    verticalScreen.style.visibility = "hidden";
+    horizontalScreen.style.visibility = "hidden";
+}
+
+PHONE_V.addEventListener('click', offVerticalScreen);
+PHONE_H.addEventListener('click', offHorizontalScreen);
+
+
+/* Portfolio */
+PORTFOLIO_MENU.addEventListener('click', (event) => {
+    PORTFOLIO_MENU.querySelectorAll('.btn').forEach(el => { el.classList.remove('active') });
+    event.target.classList.add('active');
+    let photosArray = document.querySelectorAll('div.photos > img');
+    let point = event.target.innerText;
+    switch(point){
+        case 'All':
+            photosArray[0].style.order="0";
+            photosArray[1].style.order="10";
+            photosArray[2].style.order="2";
+            photosArray[3].style.order="3";
+            photosArray[4].style.order="4";
+            photosArray[5].style.order="5";
+            photosArray[6].style.order="6";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="8";
+            photosArray[9].style.order="9";
+            photosArray[10].style.order="1";
+            photosArray[11].style.order="11";
+            break;
+        case 'Web Design':
+            photosArray[0].style.order="8";
+            photosArray[1].style.order="2";
+            photosArray[2].style.order="1";
+            photosArray[3].style.order="9";
+            photosArray[4].style.order="4";
+            photosArray[5].style.order="3";
+            photosArray[6].style.order="11";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="0";
+            photosArray[9].style.order="5";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="6";
+            break;
+        case 'Graphic Design':
+            photosArray[0].style.order="4";
+            photosArray[1].style.order="7";
+            photosArray[2].style.order="1";
+            photosArray[3].style.order="5";
+            photosArray[4].style.order="0";
+            photosArray[5].style.order="2";
+            photosArray[6].style.order="6";
+            photosArray[7].style.order="3";
+            photosArray[8].style.order="8";
+            photosArray[9].style.order="9";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="11";
+            break;
+        case 'Artwork':
+            photosArray[0].style.order="4";
+            photosArray[1].style.order="2";
+            photosArray[2].style.order="6";
+            photosArray[3].style.order="9";
+            photosArray[4].style.order="8";
+            photosArray[5].style.order="3";
+            photosArray[6].style.order="11";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="0";
+            photosArray[9].style.order="5";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="1";
+            break;
+    }
+});
+
+PHOTOS.addEventListener('click', (event) => {
+    if(event.target.classList.contains('active')) {
+        event.target.classList.remove('active');
+    }
+    else {
+        PHOTOS.querySelectorAll('img').forEach( el => el.classList.remove('active') );
+        event.target.classList.add('active');
+    }
+})
+
+/* Contact */
+SUBMIT.addEventListener('click', (event) => {
+     const subject = document.getElementById('subject').value.toString();
+     const description = document.getElementById('desc').value.toString();
+     document.getElementById('mes-query').innerHTML = 'The letter was sent';
+
+     if(subject){
+        document.getElementById('mes-about').innerHTML = subject;
+     }
+     else {
+        document.getElementById('mes-about').innerHTML = 'without subject';
+     }
+
+     if(description){
+        document.getElementById('mes-desc').innerHTML = description;
+     }
+     else {
+        document.getElementById('mes-desc').innerHTML = 'without description';
+     }
+
+     document.getElementById('message-block').classList.remove('hidden');
+})
+
+CLOSE_BUTTON.addEventListener('click', () => {
+     document.getElementById('form').reset();
+     document.getElementById('message-block').classList.add('hidden');
+});
